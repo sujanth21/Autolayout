5a7bad633e66c96bc11266e6e5a5cc36ea707085
Sujanth <sujanth1987@gmail.com>
2017-07-16 19:30:18 +1000

    Vertical constraints added

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -47,8 +47,10 @@ class ViewController: UIViewController {
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
         for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H: |[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
         
         
     }
