1bd73f0c79f91438f0e95e6561248ebfd35cdc9c
Sujanth <sujanth1987@gmail.com>
2017-07-16 19:16:24 +1000

    Constraints added

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -43,6 +43,14 @@ class ViewController: UIViewController {
         view.addSubview(label3)
         view.addSubview(label4)
         view.addSubview(label5)
+        
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H: |[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+        
     }
 
     override var prefersStatusBarHidden: Bool {
